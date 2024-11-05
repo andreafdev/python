7c4f826526c6cac26a0200686cc44d1f641266ba
Andrea Furtunato <124641425+andreafdev@users.noreply.github.com>
2024-11-05 19:56:05 -0300

    dicionarios/onjetos

---
+++ conteudos/dicionarios.py
@@ -0,0 +1,22 @@
+# Informações do leitor
+
+leitor = {
+    'nome': "Andrea",
+    'nivel': 1,
+    'pontos_livro': 0,  # Pontos de leitura acumulados
+    'experiencia': 0,    # Experiência acumulada
+    'resistencia': 10    # Resistência ao cansaço de leitura
+}
+
+# Lista de livros
+
+livros = [
+    {'titulo': 'Livro Infantojuvenil', 'complexidade': 1,
+        'paginas': 50, 'experiencia': 5},
+    {'titulo': 'Livro de Ficção', 'complexidade': 3,
+        'paginas': 200, 'experiencia': 10},
+    {'titulo': 'Livro Científico', 'complexidade': 7,
+        'paginas': 350, 'experiencia': 20},
+    {'titulo': 'Clássico da Literatura', 'complexidade': 10,
+        'paginas': 500, 'experiencia': 30}
+]
