3abbbe29efa0ec7411d20192e858e35898445db5
Andrea Furtunato <124641425+andreafdev@users.noreply.github.com>
2024-11-05 19:10:17 -0300

    conversão em python

---
+++ conteudos/conversao.py
@@ -0,0 +1,10 @@
+# conversão da string
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))  # int a frente do input
+peso = float(input('Digite seu pedo: '))  # float a frente do input
+
+
+print(nome)
+print(type(idade))
+print(type(peso))
