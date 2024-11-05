b348c95d58a7aeee5c49d3774ea39f856b6cd489
Andrea Furtunato <124641425+andreafdev@users.noreply.github.com>
2024-11-05 19:40:34 -0300

    repetições

---
+++ conteudos/listas.py
@@ -0,0 +1,16 @@
+# append - acrescenta um novo item no final da lista
+# insert - insere um novo item na posição dada
+# pop - remove e retorna o ultimo item
+# pop - remove e retorna o tem da posição
+# sort - ordena a lista
+# reverse - ordena a lista em ordem reversa
+# index - retorna a posição da primeira ocorrência do item
+# count - retorna o número de ocorrências do item
+# remove - remove a primeira ocorrência do item
+
+
+numeros = [0, 9, 8, 7, 6]
+
+print('total: ', len(numeros))
+print('menor valor: ', min(numeros))
+print('maior valor: ', max(numeros))
