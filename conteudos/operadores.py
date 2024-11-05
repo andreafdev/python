b126794fed978f1344bc4ce6cb27b77e9a28d790
Andrea Furtunato <124641425+andreafdev@users.noreply.github.com>
2024-11-05 19:14:10 -0300

    operadores

---
+++ conteudos/operadores.py
@@ -0,0 +1,10 @@
+soma = 1 + 1
+multiplicacao = 4 * 4
+divisao = 30 / 3
+potencia = 7 ** 2
+
+
+print('soma é igual a: ', soma)
+print('multiplicação é igual a: ', multiplicacao)
+print('divisão é igual a: ', divisao)
+print('potência é igual a: ', potencia)
